e356ca492b8e54afa6738d267eef12a8b4f1d67b
David-M <davidandres.moralesbeltran@gmail.com>
2026-02-20 12:54:31 -0500

    Fix: Configurando subcarpeta para despliegue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  plugins: [react()],
-  base: '/notepad/', // Asegúrate de que coincida con el nombre del repo en GitHub
+  plugins: [react(), tailwindcss()],
+  base: '/notepad/', // Asegúrate de que las barras estén presentes
 })
