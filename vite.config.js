ef0d38a4cfa886006d71029fc6e21bf598bc6f03
David-M <davidandres.moralesbeltran@gmail.com>
2026-02-20 12:28:41 -0500

    Version 20/02/2026/

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// Adaptado a tu repositorio: davesp99/notepad
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: '/notepad/', 
 })
