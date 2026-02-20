ca44df6db942f3064277d22b8b6be6a8b433a351
David-M <davidandres.moralesbeltran@gmail.com>
2026-02-20 12:46:05 -0500

    Fix: Eliminando rutas absolutas en index.html

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Cambia esto a './' para que busque los estilos 
-  // en la misma carpeta donde está el index.html
-  base: './', 
+  base: '/notepad/', // Asegúrate de que coincida con el nombre del repo en GitHub
 })
