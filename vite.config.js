c8cb45039f3978b7b9f2dd778179acf390a69e59
David-M <davidandres.moralesbeltran@gmail.com>
2026-02-20 12:57:15 -0500

    Restaurando importaciones y rutas de Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/notepad/', // Asegúrate de que las barras estén presentes
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/notepad/', 
 })
